f4ce65b6fa5f17153911cfd00b25b8c11b76e4a5
ShivamDashore16 <shivamdashore16@gmail.com>
2022-05-23 16:47:09 +0530

    added multiply.java class

--- src/com/Sample/Multiply.java
+++ src/com/Sample/Multiply.java
@@ -1,15 +1,15 @@
 package com.Sample;
 
-public class Sum {
+public class Multiply {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int a=20;
 		int b=30;
-		int c=a+b;
-		System.out.println("Sum of two numbers is "+c);
-		
+		int c=a*b;
+		System.out.println("Multiply of two numbers is "+c);
+	
 	}
 
 }
