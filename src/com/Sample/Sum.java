0eae1ee85b05f5684e5960eb5112c3b88d32c767
ShivamDashore16 <shivamdashore16@gmail.com>
2022-05-23 16:36:01 +0530

    initial commit

---
+++ src/com/Sample/Sum.java
@@ -0,0 +1,15 @@
+package com.Sample;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println("Sum of numbers is "+c);
+		
+	}
+
+}
